e6b95cb815dc81ab2df624615be95397e568209a
VerSyun <vschipstun@gmail.com>
2023-12-11 10:18:30 +0300

    beta_v0.2

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from colorama import Fore, Style
 file = '/etc/passwd'
 startCheckSum = hashlib.md5(open(file, "rb").read()).hexdigest() #изначальная хеш-сумма, по которой определяется измененение файла
 startFileSize = os.path.getsize(file) #размер нужен для того, чтобы понять, что конкретно было сделано
+startLinesNum = len(open(file, 'r').readlines())
 startLastLine = open(file, "r").readlines()[-1] #как правило, новый пользователь добавляется в конец файла, поэтому он нам понадобится
 
 while True:
     runCheckSum = hashlib.md5(open(file, "rb").read()).hexdigest() #постоянный контроль хешсуммы
+    runLinesNum = len(open(file, 'r').readlines())
     runFileSize = os.path.getsize(file) #постоянный контроль размера
     runLastLine = open(file, "r").readlines()[-1] #постоянный контроль конца
-    if (runCheckSum != startCheckSum): #замечено изменение
+    if (runCheckSum != startCheckSum) & (runLinesNum != startLinesNum): #замечено изменение
         print(Fore.RED + Style.BRIGHT + 'Файл был изменен.' + Style.RESET_ALL)
         if (runFileSize > startFileSize):
             print("Добавлен новый пользователь " + runLastLine.split(":")[0] + " имеющий ID " + runLastLine.split(":")[2])
@@ -23,6 +25,7 @@ while True:
             startCheckSum = runCheckSum
             startFileSize = runFileSize
             startLastLine = runLastLine
+            startLinesNum = runLinesNum
         elif (confirm.lower() == 'n'):
             #тут будет функционал отбирания прав
             os.system(f'passwd -l {runLastLine.split(":")[0]}')
@@ -30,8 +33,8 @@ while True:
             startCheckSum = runCheckSum
             startFileSize = runFileSize
             startLastLine = runLastLine
+            startLinesNum = runLinesNum
             #почему-то выполняется два раза
-            #надо будет посмотреть, как файл меняется
         else:
             os.system('cls||clear') 
             print(Style.BRIGHT + Fore.CYAN +'НЕДОПУСТИМЫЙ ВВОД' + Style.RESET_ALL)
@@ -39,3 +42,4 @@ while True:
     del runCheckSum
     del runFileSize
     del runLastLine
+    del runLinesNum
